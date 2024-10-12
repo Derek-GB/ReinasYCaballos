c053f96ac9c71003c090a32eacafc9a3d97a1648
DylanS.S <dsegurasevilla@gmail.com>
2024-10-12 15:52:01 -0600

    Cambios
    Se quito la vista y controlador, se agrego un JFramaReina para la interfaz y se agrego imagenes

--- reinas/logica/OchoReinasLogica.java
+++ reinas/logica/OchoReinasLogica.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package reinas.modelo;
+package reinas.logica;
 
 /**
  *
  * @author d2tod
  */
-public class OchoReinasModel {
+public class OchoReinasLogica {
 
     private static final int N = 8;
     private int cont = 0;
