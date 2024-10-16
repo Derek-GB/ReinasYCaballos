24fca0f95499d475c3831982e0204a202ee0c922
TodV <d2tod12@gmail.com>
2024-10-15 21:14:57 -0600

    Ajustes finales en los comentarios de

--- reinas/logica/OchoReinasLogica.java
+++ reinas/logica/OchoReinasLogica.java
@@ -17,49 +17,48 @@ public class OchoReinasLogica {
     private static final int N = 8;
     private int cont = 0;
     private Casilla[][] tablero;
-    private JToggleButton boton, botonC;
+    private JToggleButton botonF, botonC;
 
     public OchoReinasLogica(Casilla[][] tablero, JToggleButton boton, JToggleButton botonC) {
         this.tablero = tablero;
-        this.boton = boton;
+        this.botonF = boton;
         this.botonC = botonC;
     }
 
     public boolean resolver(int fila) {
-        return resolverColumna(fila, 0) && cont == 8;  // Comenzamos en la fila actual y en la primera columna (col = 0)
+        return resolverColumna(fila, 0) && cont == N;  // Retorna si al final la cantidad de reinas es 8
     }
 
     private boolean resolverColumna(int fila, int col) {
-        // Caso base: si ya colocamos una reina en cada fila, el problema está resuelto
+        // Caso base: si ya se colocó una reina en cada fila, el problema está resuelto
         if (fila >= N) {
             return true;
         }
 
-        // Si hemos recorrido todas las columnas y no encontramos una solución, devolvemos falso
+        // Si se recorrió todas las columnas y no se encontró solución, devuelve falso
         if (col >= N) {
             return false;
         }
 
         dormir();
 
-        // Intentamos colocar la reina en la posición (fila, col) si es seguro hacerlo
+        // Se revisa si la casilla es segura
         if (esSeguro(fila, col)) {
-            tablero[fila][col].chanceEstado(Estado.OCUPADO);  // Colocamos la reina en esta posición
+            tablero[fila][col].chanceEstado(Estado.OCUPADO);  // Se coloca una reina en la posicion
             cont++;
 
-            // Llamamos recursivamente para colocar la siguiente reina en la siguiente fila
+            // Llamada recursiva para colocar una reina en la siguiente fila
             if (resolverColumna(fila + 1, 0)) {
-                return true;  // Si encontramos una solución, devolvemos true
+                return true;  // Si se encuentra solucion, devolver true
             }
 
-            // Si no encontramos una solución, deshacemos la colocación de la reina (backtracking)
+            // Si no encontra una solución, se quita de la reina (backtracking)
             finalizadoEn(fila,col);
             tablero[fila][col].chanceEstado(Estado.VACIO);
             cont--;
         }
 
-        // Si no fue posible en la columna actual, probamos con la siguiente columna
-//        tablero[fila][col].chanceEstado(Estado.BLOQUEADO);
+        // Si no fue posible en la casilla actual, se prueba en la siguiente
         return resolverColumna(fila, col + 1);
     }
 
@@ -69,11 +68,12 @@ public class OchoReinasLogica {
 
     private boolean revisarColumna(int fila, int col) {
         if (fila < 0) {
-            return true;  // Caso base: Si hemos revisado todas las filas
+            return true;  // Caso base: Se revisaron todas las filas
         }
         if (tablero[fila][col].getEstado() == Estado.OCUPADO) {
             return false;  // Hay una reina en la misma columna
         }
+        {/*tablero[fila][col].chanceEstado(Estado.BLOQUEADO);*/}
         return revisarColumna(fila - 1, col);  // Llamada recursiva
     }
 
@@ -82,8 +82,9 @@ public class OchoReinasLogica {
             return true;  // Caso base: Fuera del tablero
         }
         if (tablero[fila][col].getEstado() == Estado.OCUPADO) {
-            return false;  // Hay una reina en la diagonal izquierda
+            return false;  // Hay una reina
         }
+        {/*tablero[fila][col].chanceEstado(Estado.BLOQUEADO);*/}
         return revisarDiagonalIzquierda(fila - 1, col - 1);  // Llamada recursiva
     }
 
@@ -92,17 +93,18 @@ public class OchoReinasLogica {
             return true;  // Caso base: Fuera del tablero
         }
         if (tablero[fila][col].getEstado() == Estado.OCUPADO) {
-            return false;  // Hay una reina en la diagonal derecha
+            return false;  // Hay una reina
         }
+        {/*tablero[fila][col].chanceEstado(Estado.BLOQUEADO);*/}
         return revisarDiagonalDerecha(fila - 1, col + 1);  // Llamada recursiva
     }
 
     private void finalizadoEn(int fila, int col){
-        boton.setText(String.valueOf(fila+1) + ':' + String.valueOf(col+1));
-        boton.setSelected(false);
+        botonF.setText(String.valueOf(fila+1) + ':' + String.valueOf(col+1));
+        botonF.setSelected(false); 
         do{
             dormir();
-        } while(!boton.isSelected() && !botonC.isSelected());
+        } while(!botonF.isSelected() && !botonC.isSelected());
     }
     
     private void dormir(){
